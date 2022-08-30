a301a44ab57185334661deddc3128f24f51188a1
Bryce Olcott <10872719@uvu.edu>
2022-08-29 19:29:52 -0600

    added all variables

--- variables.cs
+++ variables.cs
@@ -8,14 +8,37 @@ namespace MyApplication
         {
             int cherryPies = 3;
             int applePies = 5;
+            float cherryPiePrice = 8.90F;
+            double applePiePrice = 7.56D;
             int totalPies = applePies + cherryPies;
             string myName = "Bryce";
-            string resterauntName = "The Flying Pigs";
-            Console.WriteLine("There are "+cherryPies+" cherry pies, and "+applePies+" apple pies.");
-            Console.WriteLine("There is a total of "+totalPies+" pies");
+            string bakeryName = "The Flying Pigs";
+            char messageType = 'a';
+            bool isBakery = true;
+            bool customerIsHuman = true;
             
+            if (isBakery)
+            {
+                Console.WriteLine("This is message type: " + messageType);
+                Console.WriteLine("This bakery is called " + bakeryName);
+                Console.WriteLine("Welcome, " + myName+"!");
+                Console.WriteLine("There are "+cherryPies+" cherry pies, and "+applePies+" apple pies.");
+                Console.WriteLine("The cherry pies cost $" + cherryPiePrice + " each");
+                Console.WriteLine("Apple pies cost $"+applePiePrice+" each")
+                Console.WriteLine("There is a total of "+totalPies+" pies");
+                Console.WriteLine(isBakery);
+                if (customerIsHuman)
+                {
+                    Console.WriteLine("You are a human");
+                    Console.WriteLine(customerIsHuman);
+                }
+                else
+                {
+                    Console.WriteLine("you are not a human");
+                    Console.WriteLine(customerIsHuman);
+                }
 
-
+            }
         }
     }
 }
