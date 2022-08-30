4700f26e778a3b453d76d82b3dac49fef43afa68
ZeroThundr <46485292+ZeroThundr@users.noreply.github.com>
2022-08-29 19:03:36 -0600

    Created variables.cs

---
+++ variables.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cherryPies = 3;
+            int applePies = 5;
+            int totalPies = applePies + cherryPies;
+            string myName = "Bryce";
+            string resterauntName = "The Flying Pigs";
+            Console.WriteLine("There are "+cherryPies+" cherry pies, and "+applePies+" apple pies.");
+            Console.WriteLine("There is a total of "+totalPies+" pies");
+            
+
+
+        }
+    }
+}
