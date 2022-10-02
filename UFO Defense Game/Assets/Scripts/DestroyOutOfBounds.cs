3e9d4cf24ac7cbc8354fb5358212a67fb88b994c
Bryce Olcott <10872719@uvu.edu>
2022-10-02 15:59:33 -0600

    Added gameover script to out of bounds code

--- UFO Defense Game/Assets/Scripts/DestroyOutOfBounds.cs
+++ UFO Defense Game/Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,7 +6,6 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     public float topBounds = 30.00f;
     public float lowerBounds = -10.00f;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +19,11 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }else if (transform.position.z<lowerBounds){
+            if (gameObject.CompareTag("Enemy"))//if gameobject has enemy tag end destroy the player and end the game
+            {
+                Debug.Log("Game Over");
+                Destroy(GameObject.FindWithTag("Player"));
+            }
             Destroy(gameObject);
         }
     }
