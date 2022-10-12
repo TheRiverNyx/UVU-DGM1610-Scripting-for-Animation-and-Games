0072863b87957e4f0cebfd5cac85f39ab3617fd5
Bryce Olcott <10872719@uvu.edu>
2022-10-12 06:32:41 -0600

    Fixed particle system

--- UFO Defense Game/Assets/Scripts/DetectCollision.cs
+++ UFO Defense Game/Assets/Scripts/DetectCollision.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
@@ -9,21 +10,23 @@ public class DetectCollision : MonoBehaviour
     private ScoreManager scoreManager;//reference the score manager
 
     public ParticleSystem explosionParticles;
-
+    
     public int scoreToGive;//sets the score that UFOs will give
     // Start is called before the first frame update
+    
     void Start()
     {
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
-        explosionParticles = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
+        Instantiate(explosionParticles,transform.position,transform.rotation);
+        explosionParticles.transform.parent = null;
         scoreManager.IncreaseScore(scoreToGive);//Increase score amount by scoreToGive
         Destroy(gameObject);//Destroy this game object
         Destroy(other.gameObject);//destroys the object that hits the object
-        explosionParticles.Play();
     }
+    
 }
